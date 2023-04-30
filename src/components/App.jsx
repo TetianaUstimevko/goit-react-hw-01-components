143e1e32c1293750f1fce83699b0d38f7f77d747
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-04-30 19:36:13 +0300

    add task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,9 @@ import data from '../data/data';
 import { FriendList } from './FriendList';
 import friends from '../data/friends';
 
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../data/transactions';
+
 export const App = () => {
   return (
     <Container>
@@ -21,6 +24,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </Container>
   );
 };
